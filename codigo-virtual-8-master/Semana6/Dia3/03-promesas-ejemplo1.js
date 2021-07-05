5c6d1e10333edbb42524ebf34f1ddd8bd123a302
Moises Estrada Rojas <meestrada@orclesoft.com>
2021-07-05 18:17:23 -0500

    Instalacion Module

--- codigo-virtual-8-master/Semana6/Dia3/03-promesas-ejemplo1.js
+++
@@ -1,9 +0,0 @@
-axios
-	.get('https://jsonplaceholder.typicode.com/todos')
-	.then((rpta) => {
-		console.log(rpta);
-		console.log(rpta.data);
-	})
-	.catch((error) => {
-		console.log(error);
-	});
--- codigo-virtual-8-master/Semana8/05-react-effect/src/services/usuarios.js
+++
@@ -1,5 +0,0 @@
-export const getUsuarios = async () => {
-    let response = await fetch(`https://reqres.in/api/users`)
-    let rpta = await response.json();
-    return rpta.data;
-}
